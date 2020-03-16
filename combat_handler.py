375db63dbb1359dcda338f61ead1c37b8a67db4e
AndrewLim1990 <andrewlim90@gmail.com>
2020-03-15 22:41:42 -0700

    Add combat handler

---
+++ combat_handler.py
@@ -0,0 +1,54 @@
+class CombatHandler:
+    """
+    This class implements a combat handler in charge of:
+        * Rolling Initiative
+        * Turn order
+        * Resource resetting at beginning of round
+            * Movement
+            * Action
+            * Bonus Action
+    """
+    def __init__(self, environment, combatants=[]):
+        self.environment = environment
+        self.combatants = combatants
+        self.turn_order = list()
+        self.combat_is_over = False
+
+    def add_combatant(self, combatant):
+        self.combatants.append(combatant)
+
+    def remove_combatant(self, combatant):
+        self.combatants.remove(combatant)
+
+    def set_environment(self, environment):
+        self.environment = environment
+
+    def initialize_combat(self):
+        self.combat_is_over = False
+        self.roll_combat_initiative()
+
+    def roll_combat_initiative(self):
+        """
+        Determines turn order of all combatants
+        """
+        for combatant in self.combatants:
+            self.turn_order.append([combatant, combatant.roll_initiative()])
+        self.turn_order = sorted(self.turn_order, key=lambda x: x[1], reverse=True)
+
+        print("Turn order: {}".format([(c.name, initiative) for c, initiative in self.turn_order]))
+
+    def reset_resources(self):
+        pass
+
+    def check_if_combat_is_over(self):
+        pass
+
+    def run(self):
+        """
+        Runs Combat
+        """
+        self.initialize_combat()
+        while not self.combat_is_over:
+            for combatant, initiative in self.turn_order:
+                print("Turn: {}".format(combatant.name))
+            self.combat_is_over = True
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from creatures import vampire
 from actions import vampire_bite
 from actions import Move
 from environments import square_room
+from combat_handler import CombatHandler
 
 
 def main():
-    vampire.use_action(vampire_bite, target_creature=leotris)
-    vampire.use_action(Move(direction="up", distance=10, environment=square_room))
+    # vampire.use_action(Move(direction="right", distance=15, environment=square_room))
+    # vampire.use_action(vampire_bite, target_creature=leotris)
+    combat_handler = CombatHandler(environment=square_room, combatants=[leotris, vampire])
+    combat_handler.run()
 
 
 if __name__ == "__main__":
